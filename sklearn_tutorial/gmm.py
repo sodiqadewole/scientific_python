f5784eb7b25df7efe259e636c7f2d21c2cb320fb
sodiqadewole <soa2wg@virginia.edu>
2021-02-09 15:56:52 -0500

    added more experiments

--- sklearn_tutorial/gmm.py
+++ sklearn_tutorial/gmm.py
@@ -2,6 +2,14 @@
 # GMM uses EM algorithm and are often used for clustering
 # Full covariance performs best but prone to overfitting on small datasets
 # GMM as CLUSTERING
+# It is the fastest algorithm for learning mixture models
+
+# When one has insufficiently many points per mixture, estimating the covariance matrices
+#likelihood unless one regularizes the covariances artificially.
+# This algorithm will always use all the components it has access to, needing held-out
+# data or information theoretical criteria to decide how many components to use in the
+# absence of external cues.
+
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
@@ -107,3 +115,191 @@ plt.show()
 
 
 # GMM for Density Estimation
+# Plot the density estimation of a mixture of two Gaussians.
+# Data is generated from two Gaussians with different centers and covariance matrices.
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+from sklearn import mixture
+
+n_samples = 300
+
+# generate random sample, two components
+np.random.seed(0)
+
+# generate spherical data centered on (20, 20)
+shifted_gaussian = np.random.randn(n_samples, 2) + np.array([20, 20])
+
+# generate zero centered stretched Gaussian data
+C = np.array([[0., -0.7], [3.5, .7]])
+stretched_gaussian = np.dot(np.random.randn(n_samples, 2), C)
+
+# concatenate the two datasets into the final training set
+X_train = np.vstack([shifted_gaussian, stretched_gaussian])
+X_train.shape
+
+# fit a Gaussian Mixture Model with two components
+clf = mixture.GaussianMixture(n_components=2, covariance_type='full')
+clf.fit(X_train)
+
+# display predicted scores by the model as a contour plot
+x = np.linspace(-20., 30.)
+y = np.linspace(-20., 40.)
+X, Y = np.meshgrid(x, y)
+
+XX = np.array([X.ravel(), Y.ravel()]).T # flatten
+Z = -clf.score_samples(XX)
+Z = Z.reshape(X.shape)
+
+# Plots
+CS = plt.contour(X, Y, Z, norm=LogNorm(vmin=1.0, vmax=1000.0), levels=np.logspace(0, 3, 10))
+CB = plt.colorbar(CS, shrink=0.8, extend='both')
+plt.scatter(X_train[:, 0], X_train[:, 1], .8)
+plt.title('Negative log-likelihood predicted by a GMM')
+plt.axis('tight')
+plt.show()
+
+
+
+# GMM Selection¶
+import numpy as np
+import itertools
+
+from scipy import linalg
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+from sklearn import mixture
+
+# Number of samples per component
+n_samples = 500
+
+# Generate random sample, two components
+np.random.seed(0)
+C = np.array([[0., -0.1], [1.7, .4]])
+X = np.r_[np.dot(np.random.randn(n_samples, 2), C),
+          .7 * np.random.randn(n_samples, 2) + np.array([-6, 3])]
+
+lowest_bic = np.infty
+bic = []
+n_components_range = range(1, 7)
+cv_types = ['spherical', 'tied', 'diag', 'full']
+for cv_type in cv_types:
+    for n_components in n_components_range:
+        # Fit a Gaussian mixture with EM
+        gmm = mixture.GaussianMixture(n_components=n_components,
+                                      covariance_type=cv_type)
+        gmm.fit(X)
+        bic.append(gmm.bic(X))
+        if bic[-1] < lowest_bic:
+            lowest_bic = bic[-1]
+            best_gmm = gmm
+
+bic = np.array(bic)
+color_iter = itertools.cycle(['navy', 'turquoise', 'cornflowerblue',
+                              'darkorange'])
+clf = best_gmm
+bars = []
+
+# Plot the BIC scores
+plt.figure(figsize=(8, 6))
+spl = plt.subplot(2, 1, 1)
+for i, (cv_type, color) in enumerate(zip(cv_types, color_iter)):
+    xpos = np.array(n_components_range) + .2 * (i - 2)
+    bars.append(plt.bar(xpos, bic[i * len(n_components_range):
+                                  (i + 1) * len(n_components_range)], width=.2, color=color))
+plt.xticks(n_components_range)
+plt.ylim([bic.min() * 1.01 - .01 * bic.max(), bic.max()])
+plt.title('BIC score per model')
+xpos = np.mod(bic.argmin(), len(n_components_range)) + .65 +\
+    .2 * np.floor(bic.argmin() / len(n_components_range))
+plt.text(xpos, bic.min() * 0.97 + .03 * bic.max(), '*', fontsize=14)
+spl.set_xlabel('Number of components')
+spl.legend([b[0] for b in bars], cv_types)
+
+
+# Plot the winner
+splot = plt.subplot(2, 1, 2)
+Y_ = clf.predict(X)
+for i, (mean, cov, color) in enumerate(zip(clf.means_, clf.covariances_,color_iter)):
+    v, w = linalg.eigh(cov)
+    if not np.any(Y_ == i):
+        continue
+    plt.scatter(X[Y_ == i, 0], X[Y_ == i, 1], .8, color=color)
+
+    # Plot an ellipse to show the Gaussian component
+    angle = np.arctan2(w[0][1], w[0][0])
+    angle = 180. * angle / np.pi  # convert to degrees
+    v = 2. * np.sqrt(2.) * np.sqrt(v)
+    ell = mpl.patches.Ellipse(mean, v[0], v[1], 180. + angle, color=color)
+    ell.set_clip_box(splot.bbox)
+    ell.set_alpha(.5)
+    splot.add_artist(ell)
+
+plt.xticks(())
+plt.yticks(())
+plt.title(f'Selected GMM: {best_gmm.covariance_type} model, '
+          f'{best_gmm.n_components} components')
+plt.subplots_adjust(hspace=.35, bottom=.02)
+plt.show()
+
+# GMM Ellipsoids
+import itertools
+
+import numpy as np
+from scipy import linalg
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+from sklearn import mixture
+
+
+color_iter = itertools.cycle(['navy', 'c', 'cornflowerblue', 'gold', 'darkorange'])
+
+def plot_results(X, Y_, means, covariances, index, title):
+    splot = plt.subplot(2, 1, 1 + index)
+    for i, (mean, covar, color) in enumerate(zip(means, covariances, color_iter)):
+        v, w = linalg.eigh(covar)
+        v = 2. * np.sqrt(2.) * np.sqrt(v)
+        u = w[0] / linalg.norm(w[0])
+        # as the DP will not use every component it has access to
+        # unless it needs it, we shouldn't plot the redundant
+        # components.
+        if not np.any(Y_ == i):
+            continue
+        plt.scatter(X[Y_ == i, 0], X[Y_ == i, 1], .8, color=color)
+
+        # Plot an ellipse to show the Gaussian component
+        angle = np.arctan(u[1] / u[0])
+        angle = 180. * angle / np.pi  # convert to degrees
+        ell = mpl.patches.Ellipse(mean, v[0], v[1], 180. + angle, color=color)
+        ell.set_clip_box(splot.bbox)
+        ell.set_alpha(0.5)
+        splot.add_artist(ell)
+
+    plt.xlim(-9., 5.)
+    plt.ylim(-3., 6.)
+    plt.xticks(())
+    plt.yticks(())
+    plt.title(title)
+
+# Number of samples per component
+n_samples = 500
+
+# Generate random sample, two components
+np.random.seed(0)
+C = np.array([[0., -0.1], [1.7, .4]])
+X = np.r_[np.dot(np.random.randn(n_samples, 2), C),
+          .7 * np.random.randn(n_samples, 2) + np.array([-6, 3])]
+
+# Fit a Gaussian mixture with EM using five components
+gmm = mixture.GaussianMixture(n_components=5, covariance_type='full').fit(X)
+plot_results(X, gmm.predict(X), gmm.means_, gmm.covariances_, 0, 'Gaussian Mixture')
+
+# Fit a Dirichlet process Gaussian mixture using five components
+dpgmm = mixture.BayesianGaussianMixture(n_components=5, covariance_type='full').fit(X)
+plot_results(X, dpgmm.predict(X), dpgmm.means_, dpgmm.covariances_, 1,
+             'Bayesian Gaussian Mixture with a Dirichlet process prior')
+
+plt.show()
